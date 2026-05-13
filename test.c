451b99120351be6eeb05889846df17c4ce5ab5fc
RanaOsman19 <r.mohsen2397@nu.edu.eg>
2026-05-13 05:25:13 +0300

    feat: add COW test program to demonstrate copy-on-write functionality

---
+++ test.c
@@ -0,0 +1,32 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main() {
+    printf("=== COW Test ===\n");
+    
+    // Allocate a page
+    int *x = sbrk(4096);
+    *x = 100;
+    printf("Parent set x = %d at address %p\n", *x, x);
+    
+    int pid = fork();
+    
+    if(pid == 0) {
+        // Child process
+        printf("Child reads x = %d\n", *x);
+        *x = 200;
+        printf("Child wrote x = %d\n", *x);
+        exit(0);
+    } else {
+        // Parent process
+        wait(0);
+        printf("Parent reads x = %d\n", *x);
+        
+        if(*x == 100) {
+            printf("SUCCESS: COW fork works!\n");
+        } else {
+            printf("FAIL: Parent value changed to %d\n", *x);
+        }
+        exit(0);
+    }
+}
